cf7de13679119ff854f6f716aff848c275fdc4fc
herlinagustinaa <herlin_agustina@teknokrat.ac.id>
2023-01-08 01:57:08 +0700

    herlin: membuat brand_vw, dashboard_vw, discount_vw, e403, en_customer, login, notification_vw, dan outlet_vw

---
+++ Client/application/views/notification_vw.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php $this->load->view('partials/head.php'); ?>
+</head>
+<body>
+    <?php $this->load->view('partials/navbar.php'); ?>
+    <div>
+        
+    </div>
+    <?php $this->load->view('partials/footer.php'); ?>
+</body>
+
+</html>
